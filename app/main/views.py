352b6151c73ea883a42fd26d7362065b1005b62a
provodokkk <danylo.provodov@gmail.com>
2024-05-07 13:12:50 +0200

    Added automatic folder cleanup.

--- app/main/views.py
+++ app/main/views.py
@@ -6,11 +6,17 @@ import sys
 from main.forms import UploadFileForm
 from main.models import license_plates
 
-from ..number_plate_recognition.main import run_plate_recongition
+sys.path.append('../')  # folder switching
+from number_plate_recognition import main
+from number_plate_recognition.util import clear_folder
+from number_plate_recognition.paths import OUTPUT_DIR, UPLOADS_DIR
 
 
 def index(request):
     if request.method == 'POST':
+        clear_folder(UPLOADS_DIR)
+        clear_folder(OUTPUT_DIR)
+
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             fp = license_plates(file=form.cleaned_data['file'])
@@ -22,6 +28,6 @@ def index(request):
 
 
 def run_plate_recognition(request):
-    user_input = request.FILES.get('InputFile')
-    run_plate_recongition(user_input)
+    # user_input = request.FILES.get('InputFile')
+    main.run_plate_recognition()
     return render(request, 'main/index.html')

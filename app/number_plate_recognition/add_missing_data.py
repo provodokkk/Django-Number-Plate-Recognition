d929c0cdc867ac32812b977dd7e38acdae5b0a4c
provodokkk <danylo.provodov@gmail.com>
2024-05-07 13:07:24 +0200

    Added import of path variables

--- app/number_plate_recognition/add_missing_data.py
+++ app/number_plate_recognition/add_missing_data.py
@@ -2,6 +2,8 @@ import csv
 import numpy as np
 from scipy.interpolate import interp1d
 
+from paths import RESULTS_CSV_FILE_PATH, INTERPOLATED_CSV_FILE_PATH
+
 
 def interpolate_bounding_boxes(data):
     # Extract necessary data columns from input data
@@ -78,7 +80,7 @@ def interpolate_bounding_boxes(data):
 
 
 # Load the CSV file
-with open('results.csv', 'r') as file:
+with open(RESULTS_CSV_FILE_PATH, 'r') as file:
     reader = csv.DictReader(file)
     data = list(reader)
 
@@ -87,7 +89,7 @@ interpolated_data = interpolate_bounding_boxes(data)
 
 # Write updated data to a new CSV file
 header = ['frame_number', 'car_id', 'car_bbox', 'license_plate_bbox', 'license_plate_bbox_score', 'license_number', 'license_number_score']
-with open('test_interpolated.csv', 'w', newline='') as file:
+with open(INTERPOLATED_CSV_FILE_PATH, 'w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=header)
     writer.writeheader()
     writer.writerows(interpolated_data)

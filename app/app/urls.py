5e08e9482a09e7e16ab520c7a526f45220f2c01d
provodokkk <danylo.provodov@gmail.com>
2024-05-11 23:09:31 +0200

    Added serving of media files in Django urlpatterns

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('run_plate_recognition/', views.run_plate_recognition, name='run_plate_recognition'),
-]
+    path('results/', views.get_processed_file, name='results'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

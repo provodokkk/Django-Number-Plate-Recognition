417de394d092f6bee21f506a02610155a10ce21b
provodokkk <danylo.provodov@gmail.com>
2024-05-11 23:17:03 +0200

    Implemented clear_folder function, and clear_all_directories to clear the UPLOADS_DIR, OUTPUTS_DIR, and PROCESSED_FRAMES_DIR directories.

--- app/number_plate_recognition/paths.py
+++ app/number_plate_recognition/paths.py
@@ -41,3 +41,18 @@ PROCESSED_FRAMES_DIR = os.path.join(BASE_DIR, '..', 'media', 'processed_frames')
 
 def get_all_processed_frame_files_info():
     return get_files_data(PROCESSED_FRAMES_DIR)
+
+
+def clear_folder(folder_path: str) -> None:
+    """Clears all files in the specified folder."""
+    try:
+        shutil.rmtree(folder_path)
+        os.makedirs(folder_path)
+    except Exception as e:
+        print(f"An error occurred while clearing the folder: {e}")
+
+
+def clear_all_directories():
+    clear_folder(UPLOADS_DIR)
+    clear_folder(OUTPUTS_DIR)
+    clear_folder(PROCESSED_FRAMES_DIR)

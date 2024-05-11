f3f673bf0b2571e2fb820198c3c8b21bf19c2885
provodokkk <danylo.provodov@gmail.com>
2024-05-11 23:18:28 +0200

    Updated the Plate model to include a processed_frame field

--- app/main/models.py
+++ app/main/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 
-# class uploaded_files(models.Model):
-#     plate_type = models.CharField(max_length=5)
+class File(models.Model):
+    uploaded_file = models.FileField(upload_to='uploads', default=None)
+    processed_file = models.FileField(upload_to='outputs', default=None)
 
 
-class license_plates(models.Model):
-    # file_id = models.IntegerField
-    # uploaded_file = models.CharField(max_length=64, default=None, null=False)
-    file = models.FileField(upload_to='uploads', default=None)
-    plate_number = models.CharField(max_length=32, default='Unknown')
+class Plate(models.Model):
+    file = models.ForeignKey('File', on_delete=models.CASCADE)
+    frame_number = models.IntegerField(default=0)
+    plate_number = models.CharField(max_length=32, default='Error')
     accuracy = models.FloatField(default=0)
+    processed_frame = models.FileField(upload_to='outputs', default=None)
+

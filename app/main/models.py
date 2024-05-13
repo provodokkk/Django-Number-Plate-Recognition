2c6039b2f7179fb8a2e202702a11bcf46404040a
provodokkk <danylo.provodov@gmail.com>
2024-05-13 16:42:54 +0200

    Paths have been changed

--- app/main/models.py
+++ app/main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from number_plate_recognition.util import file_upload_path
 
 
 class File(models.Model):
-    uploaded_file = models.FileField(upload_to='uploads', default=None)
-    processed_file = models.FileField(upload_to='outputs', default=None)
+    uploaded_file = models.FileField(upload_to=file_upload_path, default=None)
+    processed_file = models.FileField(upload_to='buffer/outputs', default=None)
 
 
 class Plate(models.Model):
@@ -11,5 +12,5 @@ class Plate(models.Model):
     frame_number = models.IntegerField(default=0)
     plate_number = models.CharField(max_length=32, default='Error')
     accuracy = models.FloatField(default=0)
-    processed_frame = models.FileField(upload_to='outputs', default=None)
+    processed_frame = models.FileField(upload_to='buffer/outputs', default=None)
 

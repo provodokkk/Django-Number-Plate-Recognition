616d5e110f812e64706b4c4f0797375b906744c5
provodokkk <danylo.provodov@gmail.com>
2024-05-13 16:43:50 +0200

    Paths were divided into permanent and buffer

--- app/number_plate_recognition/paths.py
+++ app/number_plate_recognition/paths.py
@@ -20,15 +20,20 @@ def get_files_data(folder_path: str) -> list[dict[str, str]]:
 
 
 # Uploads
-UPLOADS_DIR = os.path.join(BASE_DIR, '..', 'media', 'uploads')
+UPLOADS_DIR = os.path.join(BASE_DIR, '..', 'media', 'buffer', 'uploads')
+UPLOADS_DIR_CONST = os.path.join(BASE_DIR, '..', 'media', 'uploads')
 
 
 def get_uploaded_file_info():
-    return get_files_data(UPLOADS_DIR)[0]
+    files_data = get_files_data(UPLOADS_DIR)
+    if files_data:
+        return files_data[0]
+    return None
 
 
 # Outputs
-OUTPUTS_DIR = os.path.join(BASE_DIR, '..', 'media', 'outputs')
+OUTPUTS_DIR = os.path.join(BASE_DIR, '..', 'media', 'buffer', 'outputs')
+OUTPUTS_DIR_CONST = os.path.join(BASE_DIR, '..', 'media', 'outputs')
 
 
 def get_output_file_info():
@@ -36,7 +41,8 @@ def get_output_file_info():
 
 
 # Processed frames
-PROCESSED_FRAMES_DIR = os.path.join(BASE_DIR, '..', 'media', 'processed_frames')
+PROCESSED_FRAMES_DIR = os.path.join(BASE_DIR, '..', 'media', 'buffer', 'processed_frames')
+PROCESSED_FRAMES_DIR_CONST = os.path.join(BASE_DIR, '..', 'media', 'processed_frames')
 
 
 def get_all_processed_frame_files_info():
@@ -52,7 +58,27 @@ def clear_folder(folder_path: str) -> None:
         print(f"An error occurred while clearing the folder: {e}")
 
 
-def clear_all_directories():
+def clear_buffer_directories():
     clear_folder(UPLOADS_DIR)
     clear_folder(OUTPUTS_DIR)
     clear_folder(PROCESSED_FRAMES_DIR)
+
+
+def move_files(source_dir: str, destination_dir: str) -> None:
+    """Move files from source directory to destination directory."""
+    files = os.listdir(source_dir)
+
+    for file in files:
+        source_path = os.path.join(source_dir, file)
+        dest_path = os.path.join(destination_dir, file)
+        shutil.move(source_path, dest_path)
+
+
+def move_all_files_to_constant_dirs():
+    move_files(UPLOADS_DIR, UPLOADS_DIR_CONST)
+    move_files(OUTPUTS_DIR, OUTPUTS_DIR_CONST)
+    move_files(PROCESSED_FRAMES_DIR, PROCESSED_FRAMES_DIR_CONST)
+
+
+
+
